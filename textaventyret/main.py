bd8cd985c7eb285cf5a3452831824ff9fd0d89ee
Theo Gidlund <theo.gidlund@elev.ntig.se>
2026-08-25 13:38:08 +0200

    Fixed variable typo

--- textaventyret/main.py
+++ textaventyret/main.py
@@ -4,12 +4,12 @@ print(f"Hej {player}")
 
 choice = input("Vars vill du gå Höger elr Vänster?? ")
 
-if choice.lower() == "Höger":
+if choice.lower() == "höger":
     print("Du drar åt Höger, där var det väldigt kallt och du frös ihjäl.")
     
 else:
     print("Du drar åt Vänster, du kommer fram till 2 färgade dörrar, blå/röd.")
-    if input("Vars går du in? (blå/röd) ").lower() == "blå"
+    if input("Vars går du in? (blå/röd) ").lower() == "blå":
         print("Du har nått slutet, med ingen belöning och bara en återvändsgränd.")
     else:
         print("Du har hittat massa potatis säckar så det räcker för livet!")
