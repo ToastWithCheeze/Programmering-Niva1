a88216819e3ecd57d00f059d8bea336e84e1c37e
Theo Gidlund <theo.gidlund@elev.ntig.se>
2026-08-25 13:27:37 +0200

    Added player choices

--- textaventyret/main.py
+++ textaventyret/main.py
@@ -1,1 +1,10 @@
 player = input("Vad heter du? ")
+
+print(f"Hej {player}")
+
+choice = input("Vars vill du gå Höger elr Vänster?? ")
+
+if choice.lower() == "Höger":
+    print("Du drar åt Höger")
+else:
+    print("Du drar åt Vänster")
