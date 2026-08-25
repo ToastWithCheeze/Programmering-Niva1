68577d90fc1dc52a7063da99bc51cb9c986d1a02
Theo Gidlund <theo.gidlund@elev.ntig.se>
2026-08-25 13:36:26 +0200

    Added context and more choices

--- textaventyret/main.py
+++ textaventyret/main.py
@@ -5,6 +5,12 @@ print(f"Hej {player}")
 choice = input("Vars vill du gå Höger elr Vänster?? ")
 
 if choice.lower() == "Höger":
-    print("Du drar åt Höger")
+    print("Du drar åt Höger, där var det väldigt kallt och du frös ihjäl.")
+    
 else:
-    print("Du drar åt Vänster")
+    print("Du drar åt Vänster, du kommer fram till 2 färgade dörrar, blå/röd.")
+    if input("Vars går du in? (blå/röd) ").lower() == "blå"
+        print("Du har nått slutet, med ingen belöning och bara en återvändsgränd.")
+    else:
+        print("Du har hittat massa potatis säckar så det räcker för livet!")
+
