14f8b9ff0e81d2fc9e4dc26c146d11c50528c4ae
Theo Gidlund <theo.gidlund@elev.ntig.se>
2026-08-26 11:35:16 +0200

    Added vänster in the if statement

--- textaventyret/main.py
+++ textaventyret/main.py
@@ -7,10 +7,11 @@ choice = input("Vars vill du gå Höger elr Vänster?? ")
 if choice.lower() == "höger":
     print("Du drar åt Höger, där var det väldigt kallt och du frös ihjäl.")
     
-else:
+elif choice.lower() == "vänster":
     print("Du drar åt Vänster, du kommer fram till 2 färgade dörrar, blå/röd.")
     if input("Vars går du in? (blå/röd) ").lower() == "blå":
         print("Du har nått slutet, med ingen belöning och bara en återvändsgränd.")
     else:
         print("Du har hittat massa potatis säckar så det räcker för livet!")
-
+else: 
+    print(f"Vafan är det här?? {choice}")
