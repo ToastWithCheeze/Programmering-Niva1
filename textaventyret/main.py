8191e855be56c9b5606a670cfb8b07fb89418fab
Theo Gidlund <theo.gidlund@elev.ntig.se>
2026-08-28 10:20:09 +0200

    Added else statement and more correct if

--- textaventyret/main.py
+++ textaventyret/main.py
@@ -9,9 +9,14 @@ if choice.lower() == "höger":
     
 elif choice.lower() == "vänster":
     print("Du drar åt Vänster, du kommer fram till 2 färgade dörrar, blå/röd.")
-    if input("Vars går du in? (blå/röd) ").lower() == "blå":
+    
+    choice = input("Vars går du in? (blå/röd) ").lower()
+    if choice == "blå":
         print("Du har nått slutet, med ingen belöning och bara en återvändsgränd.")
-    else:
+    
+    elif choice == "röd":
         print("Du har hittat massa potatis säckar så det räcker för livet!")
+    else:
+        print(f"vafan har du skrivit?? {choice}")
 else: 
     print(f"Vafan är det här?? {choice}")
