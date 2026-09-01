7534c12ed72ec387bc828665c04b5053297e567e
Theo Gidlund <theo.gidlund@elev.ntig.se>
2026-09-01 13:11:59 +0200

    Changed Variablename playerHP to PlayerHP

--- brawl/main.py
+++ brawl/main.py
@@ -1,15 +1,15 @@
 import random
 
 #PlayerName = input("What's your name? ")
-playerHP = random.randint(50,200)
+PlayerHP = random.randint(50,200)
 EnemyHP = random.randint(50,200)
 
-print(f"HP {playerHP}")
+print(f"HP {PlayerHP}")
 
 #Damages player until death
-while playerHP > 0:
-    playerHP -= random.randint(1,20)
-    if playerHP <= 0:
+while PlayerHP > 0:
+    PlayerHP -= random.randint(1,20)
+    if PlayerHP <= 0:
         print("Dead")
     else:
-        print(f"HP: {playerHP}")
+        print(f"HP: {PlayerHP}")
