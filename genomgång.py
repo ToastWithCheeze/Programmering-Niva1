ad6c14734ba263401214487509a3e5b1d8a3dc6d
Theo Gidlund <theo.gidlund@elev.ntig.se>
2026-09-15 12:52:51 +0200

    Added genomgång.py for 2026-09-15

---
+++ genomgång.py
@@ -0,0 +1,18 @@
+ålder = int(input("Hur gammal är du? "))
+
+if ålder == 17:
+    print("Du är lika gammal som de flesta i EE25.")
+
+if ålder != 43:
+    print("Du är inte lika gammal som Per!")
+else:
+    print("Du är lika gammal som Per.")
+
+if ålder <= 13:
+    print("Du är väldig ung. ")
+elif ålder <=18:
+    print("Du får inte ta körkortet.")
+elif ålder <= 20:
+    print("Du får ta körkort")
+else:
+    print("Du får handla på systembolaget.")
