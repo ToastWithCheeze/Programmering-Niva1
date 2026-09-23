e42d8c71025d4d5bdff0a99a0a1677ea59006c16
ToastWithCheeze <61846405+ToastWithCheeze@users.noreply.github.com>
2026-09-24 00:02:18 +0200

    Added function to create items

--- inventory.py
+++ inventory.py
@@ -5,9 +5,21 @@ class weapon:
         self.name = name
         self.damage = damage
         self.equipped_by = None
+#Simplier way of creating items/adding items to a list
 stick = weapon("Stick", 20)
-
 inventory.append(stick)
+bread = weapon("bread", 4)
+inventory.append(bread)
+
+#more compact way of creating items
+def create_weapon(name, damage):
+    global inventory
+    item = weapon(name, damage)
+    inventory.append(item)
+#    return item
+
+create_weapon("Potato", 8)
+
 
 print(inventory[0].name)
 
